c54c3d07f31e7a254249d6c6d6a41a8decac8deb
FelixMo42 <felix@tacocat.com>
2020-01-23 15:28:01 -0800

    html search works

---
+++ src/test.js
@@ -0,0 +1,30 @@
+let Crawler = require("sleuth/crawler/Crawler")
+
+let picoCTF = require("sleuth/CTF").picoCTF
+
+let { use, add } = new Crawler()
+
+use( ({body}) => picoCTF.findFlag(body) )
+
+use( require("sleuth/crawler/use/logger")() )
+use( require("sleuth/crawler/use/robots")() )
+use( require("sleuth/crawler/use/save")() )
+use( require("sleuth/crawler/use/html")() )
+
+let fragments = [
+    "/robots.txt",
+    "/sitemap.xml",
+    "/login.html",
+    "/login.php"
+]
+
+function search(url) {
+    add( new URL(url) )
+
+    for (let fragment of fragments) {
+        add( new URL(fragment, url) )
+        add( new URL("." + fragment, url) )
+    }
+}
+
+search("https://2019shell1.picoctf.com/problem/37868/")
